d695137dae91a3e30c3b8d27a8932726bcd3aa79
Jiwoong Kwon <43807159+creeper00@users.noreply.github.com>
2020-06-04 03:30:01 +0900

    Update proxy_1.c

--- proxy_1.c
+++ proxy_1.c
@@ -15,12 +15,35 @@ typedef struct {
     char header[MAXLINE];
 }Request;
 
+typedef struct {
+    char* obj;
+    char name[MAXLINE];
+    size_t size;
+    struct CachedItem* next;
+}CachedItem; 
+
+typedef struct {
+    size_t totalsize;
+    struct CachedItem* start;
+}CacheList;
+
+struct CacheList* clist=NULL;
+struct CacheItem* nw = NULL;
+struct CacheItem* adder = NULL;
+
 void handle_client(void* vargp);
 void initialize_struct(Request* req);
 void parse_request(char request[MAXLINE], Request* req);
 int get_from_cache(char request[MAXLINE], int clientfd);
 void get_from_server(char request[MAXLINE], int serverfd, int clientfd);
 void print_struct(Request* req);
+
+void init_cache(CacheList* list);
+//void cache_URL(CachedItem* cach, CacheList* list);
+void evict(CacheList* list);
+struct CachedItem* find(CachedItem cach, CacheList* list);
+void move_to_front(char* URL, CacheList* list);
+void cache_destruct(CacheList* list);
 
  
 int main(int argc, char** argv) {
@@ -29,6 +52,7 @@ int main(int argc, char** argv) {
         return 0;
     }
     printf("%s", user_agent_hdr);
+    init_cache(clist);
 
     int listenfd, connfd, clientlen;
     struct sockaddr_in clientaddr;
@@ -53,8 +77,9 @@ void handle_client(void* vargp) {
     int connfd = *((int*)vargp);
     Pthread_detach(pthread_self());
     int check = 0;
-    char buf[MAXLINE], request[MAXLINE], response[MAXLINE];
-    rio_t rio, srio;
+    char buf[MAXLINE], request[MAXLINE];
+    rio_t rio;
+    size_t n;
     int clientfd;
 
     Rio_readinitb(&rio, connfd);
@@ -63,10 +88,22 @@ void handle_client(void* vargp) {
     initialize_struct(rq);
     char header[MAXLINE];
     buf[0] = 0;
-    while (strncmp(buf, "\r\n", 2)) {
-        size_t n = Rio_readlineb(&rio, buf, MAXLINE);
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) > 0){
+        if (strncmp(buf, "\r\n", 2) == 0) break;
         if (check == 0) {
-            parse_request(buf, rq);
+            char name[MAXLINE];
+            char ver[MAXLINE];
+            sscanf(buf, "%s %s %s", req->method, name, ver);
+
+            if (clist != NULL) {
+                nw = find(name, clist);
+                if (nw != NULL) {
+                    move_to_front(nw, clist);
+                }
+                else adder->name = name;
+            }
+
+            parse_request(name, rq);
             sprintf(header, "Host: %s:%s\r\n",rq->hostname, rq->port);
             sprintf(header, "%s%s", header, user_agent_hdr);
             sprintf(header, "%s%s%s", header, "Connection: close\r\n", "Proxy-Connection: close\r\n");
@@ -77,7 +114,7 @@ void handle_client(void* vargp) {
         check++;
     }
 
-    sprintf(request, "%s %s %s\r\n", req->method, req->query, req->version);
+    sprintf(request, "%s %s %s\r\n", rq->method, rq->query, rq->version);
     sprintf(request, "%s%s", request, header);
 
     clientfd = Open_clientfd(rq->hostname, rq->port);
@@ -88,11 +125,8 @@ void handle_client(void* vargp) {
 }
 
 void parse_request(char request[MAXLINE], Request* req) {
-    char name[MAXLINE];
-    char ver[MAXLINE];
-    sscanf(request, "%s %s %s", req->method, name, ver);
 
-    char* tmp1 = strstr(name, "//");
+    char* tmp1 = strstr(request, "//");
     char* tmp2 = strstr(tmp1, ":");
     char* tmp3;
     if (tmp2 != NULL) tmp3 = strstr(tmp2, "/");
@@ -130,11 +164,64 @@ void initialize_struct(Request* req) {
 void get_from_server(char request[MAXLINE], int serverfd, int clientfd) {
     char response[MAXLINE];
     size_t n;
-    rio_t rio_to_server;
-    Rio_readinitb(&rio_to_server, serverfd);
-
-    Rio_writen(serverfd, request, strlen(request));
-    while ((n = Rio_readnb(&rio_to_server, response, MAXLINE)) > 0) {
-        Rio_writen(clientfd, response, n);
+    rio_t rio;
+    if (nw == NULL) {
+        Rio_readinitb(&rio, serverfd);
+        int first = 1;
+        adder->size = 0;
+        Rio_writen(serverfd, request, strlen(request));
+        while ((n = Rio_readnb(&rio, response, MAXLINE)) > 0) {
+            Rio_writen(clientfd, response, n);
+            adder->size += n;
+            if (first) sprintf(adder->obj, "%s", response);
+            else sprintf(adder->obj, "%s%s", adder->obj, response);
+        }
+        if (clist->totalsize + adder->size <= MAX_CACHE_SIZE) {
+            cache_destruct(clist);
+        }
+        adder->next = clist->start->next;
+        clist->start->next = adder;
+        clist->totalsize += adder->size;
+        adder = NULL;
     }
+    else {
+        Rio_writen(clientfd, nw->obj, nw->size);
+        nw = NULL;
+    }
+}
+
+void init_cache(CacheList* list) {
+    list->start->obj = NULL;
+    list->start->size = 0;
+    list->start->name[0] = '\0';
+    list->start->next = NULL;
+    list->totalsize = 0;
+}
+
+struct CachedItem* find(char* URL, CacheList* list) {
+    struct CacheItem* res = list->start->next;
+    while (res != NULL) {
+        if (!strcmp(res->name, URL)) return res;
+        res = res->next;
+    }
+}
+
+void move_to_front(CachedItem cach, CacheList* list) {
+    cach->next = list->start->next;
+    list->start->next = cach;
+}
+
+void evict(CacheList* list) {
+    struct CacheItem* res = list->start->next;
+    if (res == NULL) return;
+    if (res->next == NULL) {
+        list->totalsize = 0;
+        list->start->next = NULL;
+        return;
+    }
+    while (res->next->next != NULL) {
+        res = res->next;
+    }
+    list->totalsize -= res->next->size;
+    res->next = NULL;
 }
